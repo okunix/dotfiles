be11c1d8fb86ba3fc736c32fa6f8328c164f9ede
yosa12978 <yosa12978@gmail.com>
2025-10-25 10:58:49 +0300

    wezterm fix

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 
-config.font_size = 12
+config.font_size = 13
 config.font = wezterm.font({
 	family = "JetBrainsMono Nerd Font",
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
@@ -16,10 +16,10 @@ config.window_background_opacity = 0.9
 config.window_decorations = "NONE"
 config.enable_tab_bar = false
 config.window_padding = {
-	left = 25,
-	right = 25,
-	top = 25,
-	bottom = 25,
+	left = 15,
+	right = 15,
+	top = 15,
+	bottom = 13,
 }
 
 config.cursor_blink_ease_in = "Constant"
