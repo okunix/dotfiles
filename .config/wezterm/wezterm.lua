157493da5b003e4792ecb8ea1efdbdaf92543453
yusuf <yosa12978@gmail.com>
2025-10-25 10:53:06 +0300

    updated wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 
-config.font_size = 13
+config.font_size = 12
 config.font = wezterm.font({
 	family = "JetBrainsMono Nerd Font",
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
@@ -24,5 +24,7 @@ config.window_padding = {
 
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
+
+config.enable_wayland = false
 
 return config
