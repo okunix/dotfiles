1fc08def648cb5d7d559fd5cf91a2f7a4e51e85a
Yusuf Yakubov <yosa12978@gmail.com>
2026-03-10 13:39:55 +0300

    fixed treesitter version and updated stow ignore

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        tag = "v0.10.0",
         lazy = false,
         config = function()
             require "custom.plugins.treesitter"
